98fa6a1bef47086df122a6ff1de098b6ac8d822a
HanTw0 <syerizbks12@gmail.com>
2025-02-12 23:40:35 +0700

    first commit

---
+++ app/Models/Transaction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Airport extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'code',
+        'flight_id',
+        'flight_class_id',
+        'name',
+        'email',
+        'phone',
+        'number_of_passengers',
+        'promo_code_id',
+        'payment_status',
+        'subtotal',
+        'grandtotal'
+    ];
+}
